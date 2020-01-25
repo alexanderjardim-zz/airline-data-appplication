8ac31bb2d1e7523bdb40b9b36750de60c543b7ae
Alexander Ramos Jardim <alexander@volanty.com>
2020-01-25 15:16:39 -0300

    unit test setup

---
+++ test/unit/model/airline.js
@@ -0,0 +1,12 @@
+const AirlineModel = require('../../../model/airline');
+const samples = require('../../airlineSamples');
+const assert = require('assert');
+
+describe('Airlines model', () => {
+    describe('#constructor', () =>{
+        it('should create valid airline object', () =>{
+            let a = new AirlineModel(samples.validAirline);
+            assert.equal(a, true);
+        });
+    });
+});
